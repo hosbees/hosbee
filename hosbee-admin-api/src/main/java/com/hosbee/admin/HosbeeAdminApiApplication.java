7dba6f42e35115fa0805a34037bce6595b345c97
shlee8689 <shlee8689@gmail.com>
2025-03-01 13:44:42 +0900

    admin 모듈 추가

---
+++ hosbee-admin-api/src/main/java/com/hosbee/admin/HosbeeAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.hosbee.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosbeeAdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HosbeeAdminApiApplication.class, args);
+    }
+
+}
---
+++ hosbee-admin-api/src/test/java/com/hosbee/admin/HosbeeAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hosbee.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosbeeAdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
