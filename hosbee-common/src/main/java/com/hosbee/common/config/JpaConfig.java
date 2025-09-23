883831b5e2e2f29d7ee4e6776a96737fe61e6ac5
shlee8689 <shlee8689@gmail.com>
2025-09-24 01:30:12 +0900

    오류 수정

--- hosbee-common/src/main/java/com/hosbee/common/config/JpaConfig.java
+++ hosbee-common/src/main/java/com/hosbee/common/config/JpaConfig.java
@@ -4,8 +4,5 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@Configuration
-@EntityScan("com.hosbee.common.entity")
-@EnableJpaAuditing
-public class JpaConfig {
-}
+// JpaConfig는 각 모듈별로 개별 설정하므로 common에서는 제거
+// 각 API 모듈(admin-api, user-api)에서 자체 JpaConfig를 사용
