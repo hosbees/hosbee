66a83fb6f338078003eb4d2f2d422ec75fe80e27
shlee8689 <shlee8689@gmail.com>
2025-09-24 23:13:37 +0900

    [수정] 데이터베이스 접속 정보 수정

--- hosbee-admin-ui/src/main/java/com/hosbee/adminweb/HosbeeAdminUiApplication.java
+++ hosbee-admin-ui/src/main/java/com/hosbee/adminweb/HosbeeAdminUiApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.hosbee.adminweb", "com.hosbee.common"})
-@EntityScan("com.hosbee.common.entity")
-@EnableJpaRepositories(basePackages = {"com.hosbee.common.repository"})
 public class HosbeeAdminUiApplication {
 
     public static void main(String[] args) {
