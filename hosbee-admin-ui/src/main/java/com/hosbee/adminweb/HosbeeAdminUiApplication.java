34b78aaf2a9b62887b44fc0bf100f542b2b7b2e3
shlee8689 <shlee8689@gmail.com>
2025-09-24 23:31:17 +0900

    [수정] 오류수정

--- hosbee-admin-ui/src/main/java/com/hosbee/adminweb/HosbeeAdminUiApplication.java
+++ hosbee-admin-ui/src/main/java/com/hosbee/adminweb/HosbeeAdminUiApplication.java
@@ -2,10 +2,8 @@ package com.hosbee.adminweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.hosbee.adminweb", "com.hosbee.common"})
+@SpringBootApplication(scanBasePackages = {"com.hosbee.adminweb"})
 public class HosbeeAdminUiApplication {
 
     public static void main(String[] args) {
